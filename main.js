49f40b574c420a26981c46b6e2bdf76cdaa85563
DukeBas <35384117+DukeBas@users.noreply.github.com>
2021-02-17 21:39:34 +0100

    Added responsive resizing chart

--- main.js
+++ main.js
@@ -8,3 +8,59 @@ var quad = getOccurrences(4, 6);
 console.log(probabilityDistribution("2d20-2+3+1d4"));
 console.log(occurrencesToProbabilities(getOccurrencesDrop(2, 20, 1, 0)));
 console.log(occurrencesToProbabilities(getOccurrencesDrop(3, 20, 2, 0)));
+
+/**
+ * function called when body has been loaded
+ */
+function init(){
+    // creating chart
+    let ctx = document.getElementById('canvas').getContext('2d');
+
+    // temp code for tesing
+    let naming = [];
+    let temp = occurrencesToProbabilities(getOccurrencesDrop(2, 20, 1, 0));
+    for (let i = 0; i < temp.length; i++){
+        naming.push(i);
+    }
+    console.log(naming)
+
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: naming,
+            datasets: [{
+                label: '# of Votes',
+                data: temp,
+                // data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+    // adding resizing chart when window resizes
+    let canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d');
+
+    // Event handler to resize the canvas when the document view is changed
+    window.addEventListener('resize', resizeCanvas, false);
+
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        // redraw chart
+        chart.update();
+    }
+
+    //TODO fix the size and updating of the chart
+}
